21e4219974b6659daefbc6ff22bab6b13a7701ca
aqsa-ayman <aqsa_ayman@hotmail.co.uk>
2017-03-08 16:26:20 +0000

    worm image added, apples fall faster
    worms start appearing when score > 10 
    apples get faster when score > 10, > 20 and > 60

--- loopyfruit.py
+++ loopyfruit.py
@@ -1,5 +1,6 @@
 import pygame, random
 import apples
+import worms
 
 pygame.init()
 
@@ -18,9 +19,7 @@ white = (255, 255, 255)
 brown = (139,69,19)
 black = (0,0,0)
 pink = (255, 198, 222)
-
-# initialises loop
-game_exit = False
+blue = (160, 193, 247)
 
 # return a clock object to later choose frames per second of moving object
 clock = pygame.time.Clock()
@@ -41,25 +40,31 @@ basket = pygame.transform.scale(basket, (basket_width, basket_height))
 def move_basket(x, y):
     game_display.blit(basket, (x, y))
 
-# create group for class to access useful functions add and draw
-apple_group = pygame.sprite.Group()
-
 # create 3 apple instances
 apple1 = apples.Apple()
 apple2 = apples.Apple()
 apple3 = apples.Apple()
 
-apple_width = 60
-apple_height = 75
+# create 1 worm instance
+worm1 = worms.Worm()
+worm2 = worms.Worm()
+
+# create group for class to access useful functions add and draw
+apple_group = pygame.sprite.Group()
+worm_group = pygame.sprite.Group()
+apple_group.add(apple1, apple2, apple3)
+worm_group.add(worm1)
 
 # set initial apple x and y (otherwise the first 3 apples fall at same time)
-apple1.rect.centerx = random.randrange(0, 800)
+apple1.rect.centerx = random.randrange(50, 750)
 apple1.rect.centery = random.randrange(-800, -5)
-apple2.rect.centerx = random.randrange(0, 800)
+apple2.rect.centerx = random.randrange(50, 750)
 apple1.rect.centery = random.randrange(-800, -5)
-apple3.rect.centerx = random.randrange(0, 800)
+apple3.rect.centerx = random.randrange(50, 750)
 apple1.rect.centery = random.randrange(-800, -5)
 
+# initialises loop
+game_exit = False
 
 # keeps the following code going until we define game_exit = true and exit
 while not game_exit:
@@ -69,7 +74,6 @@ while not game_exit:
             game_exit = True
         key = pygame.key.get_pressed()
         # if left/right arrow key pressed move basket left/right
-
         if key[pygame.K_LEFT]:
             x_change = -13
         elif key[pygame.K_RIGHT]:
@@ -91,16 +95,22 @@ while not game_exit:
     # (x + basket_width, y + basket_height) is basket bottom right
     if (basket_x) < apple1.rect.centerx < (basket_x + basket_width) and (basket_y) < apple1.rect.centery < (basket_y + basket_height):
         apple1.rect.centery = random.randrange(-800, -5)
-        apple1.rect.centerx = random.randrange(0, 800)
+        apple1.rect.centerx = random.randrange(50, 750)
         score += 1
     if (basket_x) < apple2.rect.centerx < (basket_x + basket_width) and (basket_y) < apple2.rect.centery < (basket_y + basket_height):
         apple2.rect.centery = random.randrange(-800, -5)
-        apple2.rect.centerx = random.randrange(0, 800)
+        apple2.rect.centerx = random.randrange(50, 750)
         score += 1
     if (basket_x) < apple3.rect.centerx < (basket_x + basket_width) and (basket_y) < apple3.rect.centery < (basket_y + basket_height):
         apple3.rect.centery = random.randrange(-800, -5)
-        apple3.rect.centerx = random.randrange(0, 800)
+        apple3.rect.centerx = random.randrange(50, 750)
         score += 1
+
+    # if basket touches worm player loses a life!
+    if (basket_x) < worm1.rect.centerx < (basket_x + basket_width) and (basket_y) < worm1.rect.centery < (basket_y + basket_height):
+        worm1.rect.centery = random.randrange(-800, -5)
+        worm1.rect.centerx = random.randrange(50, 750)
+
 
     # put score on screen
     myfont = pygame.font.SysFont("monospace", 20)
@@ -108,37 +118,42 @@ while not game_exit:
     game_display.blit(score_text, (5, 5))
 
     # if score increases make the apples fall faster
-    if score > 20:
+    if score > 10:
         apple1.dy = 5
         apple2.dy = 6
-    if score > 50:
+    if score > 20:
         apple1.dy = 6
         apple2.dy = 7
         apple3.dy = 8
-    if score > 100:
+        worm1.dy = 6
+        if not worm2.alive:
+            worm_group.add(worm2)
+    if score > 60:
         apple1.dy = 7
         apple2.dy = 9
+        worm1.dy = 9
 
-    # if basket goes off screen bring it back round
+    # if basket goes off screen bring it back around
     if basket_x > width:
         basket_x = -basket_width
     elif basket_x < -basket_width:
         basket_x = width
 
-    # use sprite group function to draw the apples on the screen
-    apple_group.add(apple1, apple2, apple3)
+    # use sprite group function to draw the apples and worm on the screen
     apple_group.draw(game_display)
-    
-    # enable x and y positions using function from Apple class
+    if score > 10:
+        worm_group.draw(game_display)
+
+    # use update function from the classes to make objects "fall"
     apple1.update()
     apple2.update()
     apple3.update()
+    worm1.update()
 
     pygame.display.update()
 
     # frames per second
     clock.tick(50)
-
 
 pygame.quit
 quit()
